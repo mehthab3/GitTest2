03188845ace0d7238878861b181a5b8fd24da970
Your Name <safwanmehthab@gmail.com>
2018-04-12 15:09:22 +0530

    Modified

--- sample.c
+++ sample.c
@@ -3,8 +3,8 @@ int main()
 {
 int age;
 printf("hello world");
-printf("enter your name and age?/n");
-scanf("%f%d",&name,&age);
+printf("enter your age?/n");
+scanf("%d",&age);
 printf("Your name is %f  age is %d",name,age);
 printf("/n Thank you");
 return 0;
