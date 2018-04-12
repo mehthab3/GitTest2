a2362324a8ab07a3b2545e6a0b8c817fc0fea5bf
Your Name <safwanmehthab@gmail.com>
2018-04-12 14:50:18 +0530

    name function added

--- sample.c
+++ sample.c
@@ -3,9 +3,9 @@ int main()
 {
 int age;
 printf("hello world");
-printf("enter your age?/n");
-scanf("%d",&age);
-printf("Your age is %d",age);
+printf("enter your name and age?/n");
+scanf("%f%d",&name,&age);
+printf("Your name is %f  age is %d",name,age);
 printf("/n Thank you");
 return 0;
 }
